6733630fb02c6dd780ac2503a6566940d9a7f14a
Francesco Albanese <albanesefrancesco.af@gmail.com>
2024-01-20 16:18:51 +0000

    feat: add useSiteMetadata hook

---
+++ src/hooks/useSiteMetaData.ts
@@ -0,0 +1,21 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+export const useSiteMetadata = () => {
+  const data = useStaticQuery(graphql`
+    query SiteMetadataQuery {
+      site {
+        siteMetadata {
+          title
+          description
+          siteUrl
+        }
+      }
+    }
+  `);
+
+  return {
+    title: data.site?.siteMetadata?.title ?? "Vanessa Sangiorgio",
+    description: data.site?.siteMetadata?.description ?? "",
+    siteUrl: data.site?.siteMetadata?.siteUrl ?? "",
+  };
+};

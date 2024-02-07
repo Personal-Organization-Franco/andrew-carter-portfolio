39b5eeef0ad597327849468669f6cd815b8a7f0c
Francesco Albanese <albanesefrancesco.af@gmail.com>
2024-02-07 17:18:13 +0000

    feat: tweak watch style

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -40,7 +40,7 @@ function HeroSection() {
                 />
               </div>
               <WatchNeedles className="watch-needles absolute m-auto top-6 left-6 right-0 z-20" />
-              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
+              <div className="absolute top-[60%] left-[60%] transform -translate-x-1/2 -translate-y-1/2 w-full">
                 <GatsbyImage
                   image={watchBackround}
                   alt="watch background"
